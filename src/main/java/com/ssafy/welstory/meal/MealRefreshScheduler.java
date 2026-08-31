d5789cf4740948583a09564358e9b1784c3cad18
DUDADIDO <starfbsdud@gmail.com>
2026-08-31 10:00:19 +0900

    fix: autowire meal scheduler constructor

--- src/main/java/com/ssafy/welstory/meal/MealRefreshScheduler.java
+++ src/main/java/com/ssafy/welstory/meal/MealRefreshScheduler.java
@@ -2,6 +2,7 @@ package com.ssafy.welstory.meal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,7 @@ public class MealRefreshScheduler {
     private final MealCacheService cache;
     private final Clock clock;
 
+    @Autowired
     public MealRefreshScheduler(MealCacheService cache) {
         this(cache, Clock.system(SEOUL));
     }
